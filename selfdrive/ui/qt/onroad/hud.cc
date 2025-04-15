7756990d167570f1f5edf6a56859e43b47ce4311
willowslope <willowslope@yahoo.co.jp>
2025-04-15 23:05:53 +0900

    Make the speed blue.

--- selfdrive/ui/qt/onroad/hud.cc
+++ selfdrive/ui/qt/onroad/hud.cc
@@ -107,6 +107,7 @@ void HudRenderer::drawText(QPainter &p, int x, int y, const QString &text, int a
   QRect real_rect = p.fontMetrics().boundingRect(text);
   real_rect.moveCenter({x, y - real_rect.height() / 2});
 
-  p.setPen(QColor(0xff, 0xff, 0xff, alpha));
+//p.setPen(QColor(0xff, 0xff, 0xff, alpha));
+  p.setPen(QColor(0x80, 0x80, 0xff, alpha));
   p.drawText(real_rect.x(), real_rect.bottom(), text);
 }
